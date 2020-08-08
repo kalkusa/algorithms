0bf0efb08e66019b862d68b3677d4a24e0989e62
Arkadiusz Kałkus <kalkus@10g.pl>
2020-08-09 00:38:05 +0200

    Knights movement.

--- knights-tour/code.js
+++ knights-tour/code.js
@@ -1,2 +1,18 @@
-let position = { a8: "bN", b6: "bN" };
-let chessboard = Chessboard("chessboard", position);
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const main = async () => {
+  let position = { a8: "bN" };
+  let chessboard = Chessboard("chessboard", position);
+  await sleep(1000);
+
+  position = { b6: "bN" };
+  chessboard = Chessboard("chessboard", position);
+  await sleep(1000);
+  
+  position = { c4: "bN" };
+  chessboard = Chessboard("chessboard", position);
+};
+
+main();
