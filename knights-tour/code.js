980405cc78f47c3b5b35b45d3f9a60cfc9775b13
Arkadiusz Kałkus <kalkus@10g.pl>
2020-08-11 01:02:45 +0200

    Solution, messy but solution

--- knights-tour/code.js
+++ knights-tour/code.js
@@ -2,21 +2,46 @@ const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const convertChessboardPositionToMatrixIndices = (position) => {
+chessBoardPositionsAsIndices = {};
+
+function convertChessboardPositionToMatrixIndices(position) {
+  // if (chessBoardPositionsAsIndices[position] !== undefined) {
+  //   return chessBoardPositionsAsIndices[position];
+  // } else {
+  //   chessBoardPositionsAsIndices[position] = {
+  //     x: position.charCodeAt(0) - 97,
+  //     y: Number(position[1]) - 1,
+  //   };
+  // }
   return { x: position.charCodeAt(0) - 97, y: Number(position[1]) - 1 };
-};
+}
 
-const convertMatrixIndicesToChessboardPosition = (xyPosition) => {
+matrixIndicesFromChessboardPositions = {};
+
+function convertMatrixIndicesToChessboardPosition(xyPosition) {
+  // if (matrixIndicesFromChessboardPositions[xyPosition] !== undefined) {
+  //   return matrixIndicesFromChessboardPositions[xyPosition];
+  // } else {
+  //   matrixIndicesFromChessboardPositions[xyPosition] =
+  //     String.fromCharCode(xyPosition.x + 97) + (xyPosition.y + 1);
+  // }
   return String.fromCharCode(xyPosition.x + 97) + (xyPosition.y + 1);
-};
+}
 
-const isPositionOutsideOfTheChessBoard = (xyPosition) => {
+function isPositionOutsideOfTheChessBoard(xyPosition) {
   return (
     xyPosition.x < 0 || xyPosition.y < 0 || xyPosition.x > 7 || xyPosition.y > 7
   );
-};
+}
 
-const getPossibleKnightMoves = (currentPosition) => {
+let possibleKnightMoves = {};
+
+function getPossibleKnightMoves(currentPosition) {
+  if (possibleKnightMoves[currentPosition] !== undefined) {
+    // console.log('from memo')
+    return possibleKnightMoves[currentPosition];
+  }
+
   let currentXYPosition = convertChessboardPositionToMatrixIndices(
     currentPosition
   );
@@ -67,37 +92,50 @@ const getPossibleKnightMoves = (currentPosition) => {
     .filter((move) => !isPositionOutsideOfTheChessBoard(move))
     .map((move) => convertMatrixIndicesToChessboardPosition(move));
 
-  delete possibleMoves;
-  delete currentXYPosition;
+  possibleKnightMoves[currentPosition] = movesWhichAreNotOutsideOfTheChessboard;
   return movesWhichAreNotOutsideOfTheChessboard;
-};
+}
 
-const getPositionsFromMovesStack = (moves) => {
+function getPositionsFromMovesStack(moves) {
   let positions = {};
   for (let i = 0; i < moves.length; i++) {
     positions[moves[i]] = "bN";
   }
   return positions;
-};
+}
 
-const play = async (chessboard, moves, recursionDepth) => {
+var wrongPaths = {};
+var result = null;
+
+function play(moves, recursionDepth) {
+  if (result) {
+    return;
+  }
+  // if (recursionDepth < 45) {
+  //   await sleep(1);
+  // }
   recursionDepth++;
   // console.log("Recursion depth: %o", recursionDepth);
-  await sleep(50);
+  //await sleep(50);
 
-  // let currentPosition = moves[moves.length - 1];
+  let currentPosition = moves[moves.length - 1];
   let positions = getPositionsFromMovesStack(moves);
+
+  // console.log("Positions %o", positions);
 
   // console.log("Move number: %o", moves.length);
   // console.log("Current, last position: %o", currentPosition);
   // console.log("Moves: %o", moves);
 
-  chessboard.destroy;
-  chessboard = Chessboard("chessboard", positions);
+  // chessboard.destroy;
+  //chessboard = Chessboard("chessboard", positions);
   //chessboard.position(positions, false);
 
   if (moves.length === 64) {
     //Knights are standing on all fields, success
+    // console.log("Result: %o", moves);
+    result = true;
+    // Object.assign(result, moves);
     return;
   }
 
@@ -105,7 +143,6 @@ const play = async (chessboard, moves, recursionDepth) => {
     (possibleMove) => !positions.hasOwnProperty(possibleMove)
   );
 
-  delete positions;
   // console.log("Possible moves: %o", possibleMoves);
 
   // if (possibleMoves.length === 0) {
@@ -117,29 +154,50 @@ const play = async (chessboard, moves, recursionDepth) => {
   for (let i = 0; i < possibleMoves.length; i++) {
     // console.log("Making move");
     moves.push(possibleMoves[i]);
-    if (recursionDepth < 45) {
+    if (wrongPaths[moves.toString()] !== undefined) {
+      //console.log("Memoized path, ommiting...");
+      moves.pop();
+      //console.log('Wrong paths: %o', Object.keys(wrongPaths))
+      continue;
+    }
+    if (recursionDepth < 37) {
       console.log(
         `Verifying move ${i + 1} of ${
           possibleMoves.length
         }, recursion depth ${recursionDepth}`
       );
     }
-    await play(chessboard, moves, recursionDepth);
+    play(moves, recursionDepth);
+    if (result) {
+      return;
+    }
+    wrongPaths[moves.toString()] = true;
     moves.pop();
   }
-
-  delete possibleMoves;
-
-  return;
-};
+}
 
 const main = async () => {
   let moves = [];
   moves.push("h8");
-  let positions = getPositionsFromMovesStack(moves);
-  let chessboard = Chessboard("chessboard", positions);
-  await play(chessboard, moves, 0);
+  //let positions = getPositionsFromMovesStack(moves);
+  //let chessboard = Chessboard("chessboard", positions);
+  play(moves, 0);
+
   console.log("Final moves: %o", moves);
+  let positions = getPositionsFromMovesStack(moves);
+  console.log("Positions: %o", positions);
+  await sleep(10);
+  let intiial = { h8: "bN" };
+  let chessboard = Chessboard("chessboard", intiial);
+
+  for (let i = 0; i <= 64; i++) {
+    await sleep(300);
+    let movesAtTheMoment = moves.slice(0, i);
+    positions = getPositionsFromMovesStack(movesAtTheMoment);
+    chessboard.position(positions, true);
+  }
+
+  // chessboard = Chessboard("chessboard", positions);
 };
 
 main();
